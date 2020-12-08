26000850036a45bcc89cb09f8ce113802d5b104f
Puleeno Nguyen <puleeno@gmail.com>
2020-12-08 09:39:39 +0700

    Init project structure

---
+++ includes/ProductBundles.php
@@ -0,0 +1,19 @@
+<?php
+namespace Ramphor\ProductBundles;
+
+class ProductBundles
+{
+    protected static $instance;
+
+    public static function getInstance()
+    {
+        if (is_null(static::$instance)) {
+            static::$instance = new static();
+        }
+        return static::$instance;
+    }
+
+    protected function __construct()
+    {
+    }
+}
---
+++ ramphor-product-bundles.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: Ramphor Product Bundles
+ * Plugin URI: https://github.com/ramphor/product-bundles
+ * Author: Puleeno Nguyen
+ * Author URI: https://puleeno.com
+ * Version: 1.0.0
+ * Description: Offer product bundles
+ */
+
+use Ramphor\ProductBundles\ProductBundles;
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+$composer_autoload = sprintf( '%s/vendor/autoload.php', dirname( __FILE__ ) );
+if ( file_exists( $composer_autoload ) ) {
+	require_once $composer_autoload;
+}
+
+if ( class_exists( ProductBundles::class ) ) {
+	$GLOBALS['ramphor_product_bundles'] = ProductBundles::getInstance();
+}
